a949e53c69429a8a7afe0793e7f8a6830e103f9f
cris12gm <cris12gm@gmail.com>
2023-06-02 10:49:18 +0200

    adjustedMAx

--- bin/miRNA_bench.py
+++ bin/miRNA_bench.py
@@ -134,7 +134,7 @@ if config['diffExpr']=="True":
         os.mkdir(os.path.join(jobDir,"DE"))
     combinations = createGroupFile(annotation_df,jobDir)
 
-    if len(combinations)>15:
+    if len(combinations)>45:
         error = open(errorFile, 'a')
         error.write("<p>Your annotation file has more than 6 groups, which is the maximum of NormSeq. Please relaunch with less groups.</p><p>If the problem can not be solved, please write us indicating the jobID <a href='https://github.com/cris12gm/normSeq/issues'>here</a>.</p>")
         error.close()
@@ -228,7 +228,7 @@ for group in diffGroups:
     output.write(group+"\n")
 output.close()
 
-if len(groups)>6:
+if len(groups)>10:
     error = open(errorFile, 'a')
     error.write("<p>Your annotation file has more than 6 groups, which is the maximum of NormSeq. Please relaunch with less groups.</p><p>If the problem can not be solved, please write us indicating the jobID <a href='https://github.com/cris12gm/normSeq/issues'>here</a>.</p>")
     error.close()
