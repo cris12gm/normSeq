288785632f6ea331f5b4957b45eef68941cf848a
cris12gm <cris12gm@gmail.com>
2023-07-27 12:13:40 +0200

    middelware

--- normSeq/middleware.py
+++ normSeq/middleware.py
@@ -8,9 +8,14 @@ class MaintenanceModeMiddleware:
 
     def __call__(self, request):
         path = request.META.get('PATH_INFO', "")
+        query = request.META.get('QUERY_STRING',"")
 
-        if settings.MAINTENANCE_MODE and path!= reverse("maintenance"):
-            return render(request, 'maintenance.html')
+        if settings.MAINTENANCE_BYPASS_QUERY in query:
+            request.session['bypass_maintenance']=True
+
+        if not request.session.get('bypass_maintenance', False):
+            if settings.MAINTENANCE_MODE and path!= reverse("maintenance"):
+                return render(request, 'maintenance.html')
 
         response = self.get_response(request)
 
